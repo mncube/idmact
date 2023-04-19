88b47a18a0cf374a68b16328a51f00ead291f44a
Mackson Ncube <macksonncube@gmail.com>
2023-04-19 15:32:32 +0000

    add missing test

--- tests/testthat/test-idmact_helpers.R
+++ tests/testthat/test-idmact_helpers.R
@@ -126,13 +126,20 @@ test_that("map_scores parameter checks work", {
 })
 
 test_that("map_elongate works", {
+
+  # Use map_elongate to prep map for processing
   new_map <- map_elongate(map_raw = list(1:5, 6:10),
                           map_scale = list(20, 21))
 
+  # Test that lengths are as expected
   expect_equal(length(new_map$map_raw), length(new_map$map_scale))
 
+  # Use map_elongate on concatenation format
   new_map2 <- map_elongate(map_raw = list(c(1,2,3,4,5), c(6,7,8,9,10)),
                           map_scale = list(20, 21))
+
+  # Test that lengths are as expected
+  expect_equal(length(new_map2$map_raw), length(new_map2$map_scale))
 })
 
 test_that("map_elongate_df works", {
