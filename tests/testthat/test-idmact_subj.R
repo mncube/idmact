fac2985742b472a5ef2edd4b1e69351e5027ce93
Mackson Ncube <macksonncube@gmail.com>
2023-05-11 14:41:09 +0000

    idmact_subj sanity check tests

--- tests/testthat/test-idmact_subj.R
+++ tests/testthat/test-idmact_subj.R
@@ -60,3 +60,38 @@ test_that("idmact_subj works", {
 
 
 })
+
+
+test_that("idmact_subj sanity check: small hand calculation", {
+  raw = list(2,2,2)
+  map_raw = list(1,2,3)
+  map_scale = list(10, 20, 30)
+
+  expect_equal(idmact_subj(raw = raw,
+                           map_raw = map_raw,
+                           map_scale = map_scale)$deltas, 10)
+
+})
+
+
+test_that("idmact_subj sanity check: reverse maps give equal magnitude opposite
+           delta for symmetric constant liraw scores", {
+
+  # Test 2s and 4s
+  raw1 = list(2,2,2)
+  map_raw1 = list(1,2, 3, 4, 5)
+  map_scale1 = list(10, 20, 30, 40, 50)
+
+  raw2 = list(4,4,4)
+  map_raw2 = list(1,2, 3, 4, 5)
+  map_scale2 = list(50, 40, 30, 20, 10)
+
+
+
+  expect_equal(idmact_subj(raw = raw1,
+                           map_raw = map_raw1,
+                           map_scale = map_scale1)$deltas,
+               -idmact_subj(raw = raw2,
+                           map_raw = map_raw2,
+                           map_scale = map_scale2)$deltas)
+})
