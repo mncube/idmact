02771ecdc57387014b38dbb259cfb6411c9a262d
Mackson Ncube <macksonncube@gmail.com>
2023-05-12 14:39:09 +0000

    idmact_subj tests

--- tests/testthat/test-idmact_subj.R
+++ tests/testthat/test-idmact_subj.R
@@ -63,6 +63,8 @@ test_that("idmact_subj works", {
 
 
 test_that("idmact_subj sanity check: small hand calculation", {
+
+  # List computations
   raw = list(2,2,2)
   map_raw = list(1,2,3)
   map_scale = list(10, 20, 30)
@@ -71,27 +73,47 @@ test_that("idmact_subj sanity check: small hand calculation", {
                            map_raw = map_raw,
                            map_scale = map_scale)$deltas, 10)
 
+  # One data frame computation
+  df <- data.frame(raw = c(2,2,2),
+                   rawmap = c(1,2,3),
+                   scalemap = c(10, 20, 30))
+
+  expect_equal(idmact_subj(df = df,
+                           raw = "raw",
+                           map_raw = "rawmap",
+                           map_scale = "scalemap")$deltas, 10)
+
+  # Two data frame computation
+  df <- data.frame(raw = c(2,2,2))
+  df_map <- data.frame(rawmap = c(1,2,3),
+                       scalemap = c(10, 20, 30))
+
+  expect_equal(idmact_subj(df = df,
+                           df_map = df_map,
+                           raw = "raw",
+                           map_raw = "rawmap",
+                           map_scale = "scalemap")$deltas, 10)
+
+
 })
 
 
 test_that("idmact_subj sanity check: reverse maps give equal magnitude opposite
-           delta for symmetric constant liraw scores", {
+          delta for symmetric constant liraw scores", {
 
-  # Test 2s and 4s
-  raw1 = list(2,2,2)
-  map_raw1 = list(1,2, 3, 4, 5)
-  map_scale1 = list(10, 20, 30, 40, 50)
+            # Test 2s and 4s
+            raw1 = list(2,2,2)
+            map_raw1 = list(1,2, 3, 4, 5)
+            map_scale1 = list(10, 20, 30, 40, 50)
 
-  raw2 = list(4,4,4)
-  map_raw2 = list(1,2, 3, 4, 5)
-  map_scale2 = list(50, 40, 30, 20, 10)
+            raw2 = list(4,4,4)
+            map_raw2 = list(1,2, 3, 4, 5)
+            map_scale2 = list(50, 40, 30, 20, 10)
 
-
-
-  expect_equal(idmact_subj(raw = raw1,
-                           map_raw = map_raw1,
-                           map_scale = map_scale1)$deltas,
-               -idmact_subj(raw = raw2,
-                           map_raw = map_raw2,
-                           map_scale = map_scale2)$deltas)
+            expect_equal(idmact_subj(raw = raw1,
+                                     map_raw = map_raw1,
+                                     map_scale = map_scale1)$deltas,
+                         -idmact_subj(raw = raw2,
+                                      map_raw = map_raw2,
+                                      map_scale = map_scale2)$deltas)
 })
