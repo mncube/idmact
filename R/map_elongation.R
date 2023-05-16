8dd72af50afca44bdb2be001ca1221b7f19aece0
Mackson Ncube <macksonncube@gmail.com>
2023-05-16 23:39:51 +0000

    update map_elongation documentation

--- R/map_elongation.R
+++ R/map_elongation.R
@@ -1,17 +1,26 @@
-#' Elongate Map
+#' Elongate Mapping of Raw Scores to Scale Scores
 #'
-#' @param map_raw A list containing the domain of raw scores for the raw-to-scale
-#' score mapping
-#' @param map_scale A list containing the domain of raw scores for the raw-to-scale
-#' score mapping
+#' This function elongates the mapping of raw scores to scale scores. Each element
+#' in 'map_raw' and 'map_scale' is repeated according to the length of the
+#' corresponding 'map_raw' element, resulting in equal-length lists of map_raw'
+#' and 'map_scale' scores.
 #'
-#' @return A nested list consisting of a list of raw scores and a list of scale
+#' @param map_raw A list where each element is a number or a numeric vector
+#' representing a set of raw scores. Each vector in 'map_raw' should have a
+#' corresponding element in 'map_scale'.
+#' @param map_scale A list where each element represents a scale score
+#' corresponding to the raw scores in 'map_raw'. Each element in 'map_scale' is
+#' repeated times the length of the corresponding element in 'map_raw'.
+#'
+#' @return A named list with two components: 'map_raw' and 'map_scale'. Each
+#' component is a list representing the elongated mapping of raw scores to scale
 #' scores.
 #' @export
 #'
 #' @examples
+#' # Elongate the mapping of raw scores (1:5 and 6:10) to scale scores (20 and 21)
 #' map_elongate(map_raw = list(1:5, 6:10),
-#' map_scale = list(20, 21))
+#'              map_scale = list(20, 21))
 map_elongate <- function(map_raw, map_scale) {
   # Initialize empty lists for elongated_map_raw and elongated_map_scale
   elongated_map_raw <- list()
@@ -43,25 +52,36 @@ map_elongate <- function(map_raw, map_scale) {
   return(list(map_raw = elongated_map_raw, map_scale = elongated_map_scale))
 }
 
-
-#' Elongate df_map
+#' Elongate Data Frame Mapping of Raw Scores to Scale Scores
+#'
+#' This function takes a data frame that maps raw scores to scale scores, and
+#' elongates it. Each raw score range in the 'map_raw' column is split into
+#' individual scores, and each corresponding scale score in the 'map_scale'
+#' column is repeated for each individual raw score. The function returns
+#' a new data frame with each raw score paired with its corresponding scale score.
 #'
 #' @param df_map A data frame that maps raw scores to their corresponding scale
-#' scores
-#' @param map_raw A quoted column name from df_map containing the domain of raw
-#' scores for the raw-to-scale score mapping
-#' @param map_scale A quoted column name from df_map containing the range of
-#' scale scores for the raw-to-scale score mapping
+#' scores. The 'map_raw' column should contain ranges of raw scores as strings
+#' (in the form, "1-5"), and the 'map_scale' column should contain the
+#' corresponding scale scores.
+#' @param map_raw A string representing the column name in 'df_map' that contains
+#' the raw score ranges.
+#' @param map_scale A string representing the column name in 'df_map' that contains
+#' the scale scores.
 #'
-#' @return A data frame
+#' @return An elongated data frame where each row represents a raw score and its
+#' corresponding scale score.
 #' @export
 #'
 #' @examples
+#' # Create a data frame mapping raw score ranges to scale scores
 #' df_map <- data.frame(
 #'  raw = c("1-5", "6-10"),
-#'  scale = c(10, 11))
+#'  scale = c(10, 11)
+#' )
 #' # Elongate the data frame
 #' elongated_df <- map_elongate_df(df_map, "raw", "scale")
+#' print(elongated_df)
 map_elongate_df <- function(df_map, map_raw, map_scale) {
   # Ensure that the raw_col and scale_col exist in the data frame
   if (!(map_raw %in% colnames(df_map)) || !(map_scale %in% colnames(df_map))) {
